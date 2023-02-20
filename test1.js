4b03658a20fa957be2baf5a3eeccb219cc490571
antonovvlad <mega-antoniomargaretti@ukr.net>
2023-02-20 20:29:42 +0300

    first try

--- test1.js
+++ test1.js
@@ -1,26 +1,5 @@
-var name = prompt("Your name", "name");
-if (typeof name === 'string' || myVar instanceof String){
-    var age = parseInt(prompt("Your age", "0"));
-    if (age < 18) {
-        console.log("too young");
-    } else if (age >= 80) {
-        console.log("too old");
-    } else if (age === "" || age === null || age === NaN || age === undefined) {
-        console.log("acess denided");
-    } else {
-        console.log("Welcome " + name +"!"); 
-    }    
-} else if (name === null || name == undefined) {
-    console.log("acess denided");
-} else {
-    console.log("acess denided");
-}
-
-
-
 var age = parseInt(prompt("Your age", "0"));
-
-if (age == 0) {
+if (age === 0) {
     console.log("enter something");
 } else if (age < 18) {
     console.log("too young");
@@ -36,3 +15,4 @@ if (age == 0) {
 } else {
     console.log("its may be a number")
 }
+
